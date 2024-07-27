d508b64752252b3d5e4e831a25afd7ccf8214bcb
Evgenii Perminov <perminov.eugene@gmail.com>
2024-07-27 11:37:45 +0300

    Fix docker compose

--- packages/pnft-market-etherium/hardhat.config.ts
+++ packages/pnft-market-etherium/hardhat.config.ts
@@ -2,6 +2,8 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import envConfig from "./scripts/config";
 
+console.log("---->", envConfig);
+
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
   networks: {
